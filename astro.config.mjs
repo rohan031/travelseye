568a2b32229d51c57747a6f9ffa61f04f91c689a
rohan <rohanverma031@gmail.com>
2024-01-21 06:43:56 +0530

    domain updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://rohan031.github.io",
+	site: "https://traveleyes.in",
 });
