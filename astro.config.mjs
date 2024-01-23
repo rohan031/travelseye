65e7edc840d166472f3a3fd1dd7fbf01a791f17d
rohan <rohanverma031@gmail.com>
2024-01-23 13:24:51 +0530

    added google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,21 @@ import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 
+import partytown from "@astrojs/partytown";
+
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify(),
-  integrations: [react(), icon()]
+	output: "server",
+	adapter: netlify(),
+	integrations: [
+		react(),
+		icon(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
 });
