9dca7d4e45324723a99d5aed53609ba9eaedfb76
rohan <rohanverma031@gmail.com>
2024-01-27 00:47:21 +0530

    added modals

--- src/components/React/Form/Form.tsx
+++ src/components/React/Form/Form.tsx
@@ -1,7 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./form.module.scss";
 
 const packages = ["Ticket", "Booking", "Package", "Trip"];
+
+function Loader() {
+	return (
+		<>
+			<div className="lds-ellipsis">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</>
+	);
+}
 
 export default function Form() {
 	const [details, setDetails] = useState({
@@ -11,6 +24,10 @@ export default function Form() {
 		business: "",
 		package: "0",
 	});
+	const modalRef = useRef<HTMLDialogElement | null>(null);
+	const [modalMsg, setModalMsg] = useState<string>(
+		"Thank you for submitting the form! Your information has been successfully received. We appreciate your time and will get back to you as soon as possible. Have a great day! 😊"
+	);
 
 	const [loading, setLoading] = useState(false);
 
@@ -36,108 +53,145 @@ export default function Form() {
 			package: packages[+details.package],
 		};
 
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", "/.netlify/functions/form");
-		xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-		xhr.send(JSON.stringify(inputDetails));
+		let body = JSON.stringify(inputDetails);
 
-		xhr.onload = function () {
-			const response = JSON.parse(xhr.responseText);
+		fetch("/.netlify/functions/form", {
+			method: "POST",
+			body,
+			headers: {
+				"Content-type": "application/json; charset=UTF-8",
+			},
+		})
+			.then((res) => {
+				console.log(res.status);
 
-			if (xhr.status === 200) {
-				// The request was successful
-				console.log(response);
-				console.log("success");
-				alert("Successfully received your information.");
-			} else {
-				// The request failed
-				console.log("fail");
-				alert("Can't process your req right now");
-			}
+				if (res.status !== 200)
+					throw new Error("Error getting details");
 
-			setDetails({
-				name: "",
-				email: "",
-				mobile: "",
-				business: "",
-				package: "0",
+				setModalMsg(
+					"Thank you for submitting the form! Your information has been successfully received. We appreciate your time and will get back to you as soon as possible. Have a great day! 😊"
+				);
+			})
+			.catch((err) => {
+				console.error(err);
+				setModalMsg(
+					"We're sorry, but there was an issue with your form submission. Please try again after some time. 😓"
+				);
+			})
+			.finally(() => {
+				// setDetails({
+				// 	name: "",
+				// 	email: "",
+				// 	mobile: "",
+				// 	business: "",
+				// 	package: "0",
+				// });
+
+				modalRef.current?.showModal();
+				setLoading(false);
 			});
-			setLoading(false);
-		};
+	};
+
+	const handleClose = () => {
+		modalRef.current?.close();
 	};
 
 	return (
-		<div className={styles.contact_from}>
-			<form onSubmit={handleSubmit}>
-				<h3>Enquire Now</h3>
+		<>
+			<>
+				<dialog className={styles.modal} ref={modalRef}>
+					<div className={styles.modal}>
+						<div className={styles.menu}>
+							<img src="/logo-sm.svg" alt="" />
 
-				<input
-					type="text"
-					placeholder="Name"
-					required
-					value={details.name}
-					name="name"
-					onChange={handleChange}
-					disabled={loading}
-				/>
+							<button onClick={handleClose}>
+								<img src="/close.svg" alt="" title="close" />
+							</button>
+						</div>
 
-				<input
-					type="email"
-					placeholder="Email"
-					required
-					value={details.email}
-					name="email"
-					onChange={handleChange}
-					disabled={loading}
-				/>
+						<div className={styles.text}>
+							<p>{modalMsg}</p>
+						</div>
 
-				<input
-					type="number"
-					placeholder="Mobile"
-					min={1000000000}
-					max={9999999999}
-					value={details.mobile}
-					required
-					name="mobile"
-					onChange={handleChange}
-					disabled={loading}
-				/>
+						<div className={styles.button}>
+							<button onClick={handleClose}>Okay</button>
+						</div>
+					</div>
+				</dialog>
+			</>
 
-				<input
-					type="text"
-					placeholder="Business Name"
-					value={details.business}
-					name="business"
-					onChange={handleChange}
-					disabled={loading}
-				/>
+			<div className={styles.contact_from}>
+				<form onSubmit={handleSubmit}>
+					<h3>Enquire Now</h3>
 
-				<select
-					required
-					value={details.business}
-					name="package"
-					onChange={handleChange}
-					disabled={loading}
-				>
-					<option value="-1" disabled>
-						What Are You Looking For?
-					</option>
-					<option value="0">Ticket</option>
-					<option value="1">Booking</option>
-					<option value="2">Package</option>
-					<option value="3">Trip</option>
-				</select>
+					<input
+						type="text"
+						placeholder="Name"
+						required
+						value={details.name}
+						name="name"
+						onChange={handleChange}
+						disabled={loading}
+					/>
 
-				<p className={styles.disclaimer}>
-					Disclaimer: By clicking submit, you agree to receive
-					communications from the business in accordance with our
-					Privacy Policy .
-				</p>
+					<input
+						type="email"
+						placeholder="Email"
+						required
+						value={details.email}
+						name="email"
+						onChange={handleChange}
+						disabled={loading}
+					/>
 
-				<button type="submit" disabled={loading}>
-					{loading ? "Submitting..." : "Submit"}
-				</button>
-			</form>
-		</div>
+					<input
+						type="number"
+						placeholder="Mobile"
+						min={1000000000}
+						max={9999999999}
+						value={details.mobile}
+						required
+						name="mobile"
+						onChange={handleChange}
+						disabled={loading}
+					/>
+
+					<input
+						type="text"
+						placeholder="Business Name"
+						value={details.business}
+						name="business"
+						onChange={handleChange}
+						disabled={loading}
+					/>
+
+					<select
+						required
+						value={details.business}
+						name="package"
+						onChange={handleChange}
+						disabled={loading}
+					>
+						<option value="-1" disabled>
+							What Are You Looking For?
+						</option>
+						<option value="0">Ticket</option>
+						<option value="1">Booking</option>
+						<option value="2">Package</option>
+						<option value="3">Trip</option>
+					</select>
+
+					<p className={styles.disclaimer}>
+						Disclaimer: By clicking submit, you agree to receive
+						communications from the business in accordance with our
+						Privacy Policy .
+					</p>
+
+					<button type="submit" disabled={loading}>
+						{loading ? <Loader /> : "Submit"}
+					</button>
+				</form>
+			</div>
+		</>
 	);
 }
